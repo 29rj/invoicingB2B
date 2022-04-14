fb8871f6fb2ed6bebabae838e97659effbb3bb23
29rj <rishabhrr8253@gmail.com>
2022-04-14 10:51:18 +0530

    Almost Done

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 const express = require('express');
 const path = require('path');
-const port = 6969;
+const port = 7777;
 const app = express();
 const bodyParser = require('body-parser')
-const db = require('./models/users')
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-
-// app.get('/', (request, response) => {
-//     response.json({ info: 'Node.js, Express, and Postgres API' });
-// });
 
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
       extended: true,
     }))
-
 app.use('/',require('./routes'));
-
-// app.get('/get',db.getUsers);
-// app.post('/post',db.postUsers);
-
-
 
 app.listen(port,function(err){
     if(err){
